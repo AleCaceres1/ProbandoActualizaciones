ef49e1a5fccc536a4324e774b081e3cc4f0a2577
AleCaceres1 <man2019mc@hotmail.com>
2024-11-12 16:16:22 -0600

    Versión 1.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,24 +29,75 @@
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lb_current = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(31, 119);
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(187, 65);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(743, 182);
+			this.label1.Size = new System.Drawing.Size(401, 102);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "PROBANDO \r\nACTUALIZACION 1";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(176, 341);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(403, 57);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Buscar Actualizaciones";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(187, 199);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 51);
+			this.label2.TabIndex = 2;
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// lb_current
+			// 
+			this.lb_current.AutoSize = true;
+			this.lb_current.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lb_current.Location = new System.Drawing.Point(187, 189);
+			this.lb_current.Name = "lb_current";
+			this.lb_current.Size = new System.Drawing.Size(192, 51);
+			this.lb_current.TabIndex = 3;
+			this.lb_current.Text = "Versión: ";
+			this.lb_current.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(350, 252);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 51);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "A";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.lb_current);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -58,6 +109,10 @@
 		#endregion
 
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lb_current;
+		private System.Windows.Forms.Label label3;
 	}
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,70 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
 
 namespace ProbandoActualizaciones
 {
 	public partial class Form1 : Form
 	{
+
+		string currentVersion = "1.0";
+		string versionUrl = "https://raw.githubusercontent.com/AleCaceres1/ProbandoActualizaciones/refs/heads/main/version.json";
+
 		public Form1()
 		{
 			InitializeComponent();
+			lb_current.Text = "Versión: " + currentVersion;
+		}
+		public void CheckForUpdates(string url, string currentVersion)
+		{
+			using (var cliente = new WebClient())
+			{
+				try
+				{
+					// Descargar archivo de versión desde GitHub
+					var versionData = cliente.DownloadString(versionUrl);
+					JObject versionJson = JObject.Parse(versionData);
+
+					string nuevaVersion = versionJson["version"].ToString();
+					string downloadUrl = versionJson["url"].ToString();
+
+					if (nuevaVersion != currentVersion)
+					{
+						MessageBox.Show("Nueva versión"+ nuevaVersion +"disponible. Iniciando actualización...");
+
+						// Descargar nueva versión
+						//string tempFilePath = Path.Combine(Path.GetTempPath(), "miapp_v" + nuevaVersion + ".zip");
+						//cliente.DownloadFile(downloadUrl, tempFilePath);
+
+						// Aquí puedes extraer el archivo y reemplazar los archivos de tu aplicación actual
+
+						// También puedes lanzar un actualizador independiente que realice el reemplazo.
+						//MessageBox.Show("Actualización descargada. Procede a instalar.");
+					}
+					else
+					{
+						MessageBox.Show("Ya tienes la última versión.");
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error al verificar la actualización: " + ex.Message);
+				}
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			CheckForUpdates(versionUrl, currentVersion);
 		}
 	}
-}
+	}
+
+
